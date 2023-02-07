fc1dbce8f9e12fd9166166e3670135e83c000749
Arthur D Siu <51488740+arthurdsiu@users.noreply.github.com>
2023-02-06 20:48:00 -0800

    Load (#1)
    Loaded the input file into a numpy array for use later one.
    * create empty np array
    * board loaded

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import sys
 from sokoban_reader import read_sokoban
 
 if __name__ == '__main__':
-    n_boxes = read_sokoban(input("Please enter file location: "))["n_boxes"]
-    print(f"The number of boxes in this sokoban is {n_boxes}")
+    if len(sys.argv) == 1:
+        result = read_sokoban(input("Please enter file location: "))
+    else:
+        result, board = read_sokoban(sys.argv[1])
+    print(result)
+    print(board)
+    #n_boxes = result["n_boxes"]
+    #print(f"The number of boxes in this sokoban is {n_boxes}")
--- sokoban_reader.py
+++ sokoban_reader.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def read_sokoban(input_location: str) -> dict:
     """Reads sokoban file"""
     try:
@@ -7,6 +9,14 @@ def read_sokoban(input_location: str) -> dict:
         print("Error Accessing File")
         return
 
+    input_split = list()
+    for line in input_lines:
+        row = list()
+        for num in line.split():
+            row.append(int(num))
+        input_split.append(row)        
+    #print(input_split)
+    #print(f"Sokoban board size: {input_split[0][0]} {input_split[0][1]} ")
     try:
         result = dict()
         result["size"] = [int(i) for i in input_lines[0].split(" ")]
@@ -19,4 +29,39 @@ def read_sokoban(input_location: str) -> dict:
         result["initlocation"] = [int(i) for i in input_lines[4].split(" ")]
     except Exception as E:
         print(f"Error Parsing File\n{E}")
-    return result
+
+    WALL = 1
+    BOXES = 2
+    STORAGE = 3
+    PLAYER_LOCATION = 4
+    try:
+        sokoban_board = np.zeros( (input_split[0][0], input_split[0][1]), dtype=int )
+        print("\nwall")
+        for i in range(1, 2*(input_split[WALL][0]) + 1, 2):
+            y = input_split[WALL][i] - 1
+            x = input_split[WALL][i+1] - 1
+            print(f"{i},{i+1},{x},{y}")
+            sokoban_board[y][x] = WALL
+
+        print("\nboxes")
+        for i in range(1, 2*(input_split[BOXES][0]) + 1, 2):
+            y = input_split[BOXES][i] - 1
+            x = input_split[BOXES][i+1] - 1
+            print(f"{i},{i+1},{x},{y}")
+            sokoban_board[y][x] = BOXES
+
+        print("\nstorage")
+        for i in range(1, 2*(input_split[STORAGE][0]) + 1, 2):
+            y = input_split[STORAGE][i] - 1 
+            x = input_split[STORAGE][i+1] - 1
+            print(f"{i},{i+1},{x},{y}")
+            sokoban_board[y][x] = STORAGE 
+
+        y = input_split[PLAYER_LOCATION][0] - 1
+        x = input_split[PLAYER_LOCATION][1] - 1
+        sokoban_board[y][x] = PLAYER_LOCATION
+
+    except Exception as E:
+        print(f"Error converting to np array\n{E}")
+
+    return result, sokoban_board
