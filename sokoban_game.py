2cdc8a0cc815e821f771520abc3761f1edba2103
Arthur D Siu <51488740+arthurdsiu@users.noreply.github.com>
2023-02-06 20:52:35 -0800

    basic game class

---
+++ sokoban_game.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+class Sokoban:
+    board = np.array()
+    playerPosition = (0,0)
+    
+    def __init__(self, board, playerPosition):
+        self.board = board
+        self.playerPosition
+    
+    def movePlayer(direction):
+        return False
+    
+    def checkVictory():
+        return False
+    
+    def boxesRemaining():
+        return 999
