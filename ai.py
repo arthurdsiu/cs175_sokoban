32e7b83dd1f4b38bee61602c806d2922f5b75622
Arthur D Siu <51488740+arthurdsiu@users.noreply.github.com>
2023-02-17 22:21:23 -0800

    debugging in process checkpoint

--- ai.py
+++ ai.py
@@ -2,6 +2,7 @@ import numpy as np
 import globals as g
 import time
 import sokoban_game
+from BitVector import BitVector
 
 '''This class detects deadlocks. Grids that are deadzone will be marked
 with g.DEADLOCK'''
@@ -28,7 +29,6 @@ class AI:
     board = np.zeros((0,0))
     startPosition = None
     goals = None
-
     
     def __init__(self, nparray):
         game = sokoban_game.Sokoban(nparray)
@@ -41,6 +41,7 @@ class AI:
         startTime = time.time()
         path = list()
         board = self.board
+        visitedStates = dict()
 
         def finished(board):
             for goal in self.goals:
@@ -48,67 +49,71 @@ class AI:
                     return False
             return True        
 
-        def dfs(move):
+        def dfs(newPosition):
             if time.time() - startTime  > g.TIME_LIMIT:
                 return False
 
             if finished(board):
                 return True
-            if move[2] == g.UP:
-                position=(move[0]-1,move[1])
-            if move[2] == g.DOWN:
-                position=(move[0]+1,move[1])
-            if move[2] == g.LEFT:
-                position=(move[0],move[1]-1)
-            if move[2] == g.RIGHT:
-                position=(move[0],move[1]+1)
-
-            moves = self.generateAllMoves(position,board)
+            moves, normalized = self.generateAllMoves(newPosition, board)
+            print(f"Possible moves {moves}")
+            print("Board state, all possible moves marked with a '.' ")
+            printBoard(board,moves, newPosition)
+            
+            # I"m not sure about this
+            key = self.getState(board, normalized)
+            if visitedStates.setdefault(key):
+                return False
+            visitedStates[key] = True
 
             for move in moves:
                 path.append(move)
                 if move[2] == g.UP:
-                    board[move[0]+2,move[1]] = g.BOXES
-                    board[move[0]+1,move[1]] = g.EMPTY
-                if move[2] == g.DOWN:
                     board[move[0]-2,move[1]] = g.BOXES
                     board[move[0]-1,move[1]] = g.EMPTY
+                    newPos = [move[0]-1,move[1]]
+                if move[2] == g.DOWN:
+                    board[move[0]+2,move[1]] = g.BOXES
+                    board[move[0]+1,move[1]] = g.EMPTY
+                    newPos =[move[0]+1,move[1]] 
                 if move[2] == g.LEFT:
                     board[move[0],move[1]-2] = g.BOXES
                     board[move[0],move[1]-1] = g.EMPTY
+                    newPos = [move[0],move[1]-1]
                 if move[2] == g.RIGHT:
                     board[move[0]+2,move[1]+2] = g.BOXES
                     board[move[0]+1,move[1]+1] = g.EMPTY
+                    newPos = [move[0]+1,move[1]+1]
+                print(f"Box moved {move} \nhere's board state")
+                printBoard(board,None, newPos)
 
-
-                if dfs(move):
+                if dfs(newPos):
                     return True
 
                 path.pop()
                 if move[2] == g.UP:
-                    board[move[0]+2,move[1]] = g.BOXES
-                    board[move[0]+1,move[1]] = g.EMPTY
+                    board[move[0]-2,move[1]] = g.EMPTY
+                    board[move[0]-1,move[1]] = g.BOXES
                 if move[2] == g.DOWN:
-                    board[move[0]-2,move[1]] = g.BOXES
-                    board[move[0]-1,move[1]] = g.EMPTY
+                    board[move[0]+2,move[1]] = g.EMPTY
+                    board[move[0]+1,move[1]] = g.BOXES
                 if move[2] == g.LEFT:
-                    board[move[0],move[1]-2] = g.BOXES
-                    board[move[0],move[1]-1] = g.EMPTY
+                    board[move[0],move[1]-2] = g.EMPTY
+                    board[move[0],move[1]-1] = g.BOXES
                 if move[2] == g.RIGHT:
-                    board[move[0]+2,move[1]+2] = g.BOXES
-                    board[move[0]+1,move[1]+1] = g.EMPTY
+                    board[move[0]+2,move[1]+2] = g.EMPTY
+                    board[move[0]+1,move[1]+1] = g.BOXES
                 position = (move[0],move[1])
 
             return False
 
-        moves = self.generateAllMoves(self.startPosition, board)
-        for move in moves:
-            if dfs(move):
-                return path
-        return None
-    
+        return dfs(self.startPosition)
     '''
     returns a list of moves in [y,x,direction]
+    (y,x) is front of a box
+    the box will be the new location of the player
+    also return the normalized position of the player for
+    use it state space based on position parameter passed to 
     '''
     def generateAllMoves(self, position, board):
         #find connected whitespace
@@ -119,7 +124,7 @@ class AI:
         visitedBoxes = np.zeros((board.shape))
 
         def isValidConnectedSpace(cur):
-            if 0 <= cur[0] < board.shape[0] and 0 <= cur[1] < board.shape[1]:
+            if cur != None and  0 <= cur[0] < board.shape[0] and 0 <= cur[1] < board.shape[1]:
                 if board[cur[0]][cur[1]] == g.BOXES:
                     if visitedBoxes[cur[0]][cur[1]]:
                         return False
@@ -150,39 +155,64 @@ class AI:
             visited[cur[0]][cur[1]]=1
         visit(cur)
 
-        #return boxes
+        minElemY = min(connectedSpace, key=lambda x: x[0])
+        normalized = min(filter( lambda x: x[0]== minElemY[0], connectedSpace), key=lambda x: x[1])
         #found all connected space
         #now we need to find all neighboring boxes of the connected space
         pushable = [] #check whether box can be pushed
-        pushVisited = np.zeros((board.shape[0],board.shape[1],4))
+        
+        # print(f"normalized:{normalized}")
         for box in boxes:
             neighbors = self.getNeigbors(box)
-            # print(f"Box{box} neribors:{neighbors}")
+            #print(f"Box{box} neribors:{neighbors}")
             opposite = [1,0,3,2]
             for i,n in enumerate(neighbors):
                 # print(f"neigbor:{n}, visited: {visited[n[0]][n[1]]}, oppositEmpty: {isEmpty(neighbors[opposite[i]])}")
-                if pushVisited[n[0]][n[1]][opposite[i]]:
-                        continue
-                if visited[n[0]][n[1]] and isEmpty(neighbors[opposite[i]]):
-                    n.append(opposite[i])
+                if n == None:
+                    continue
+                if visited[n[0]][n[1]] and neighbors[opposite[i]]!= None and isEmpty(neighbors[opposite[i]]):
+                    n.append(opposite[i]) # if n is valid, and you're UP, that means you need to push DOWN
                     pushable.append(n)
-                    pushVisited[n[0]][n[1]][[opposite[i]]]=1
-        print(pushable)
+                    
+        #print(pushable)
+        return pushable, normalized
 
     def getNeigbors(self, tile):
         #up, down, left, right
-        return [[tile[0]+1, tile[1]], [tile[0]-1, tile[1]], [tile[0], tile[1]-1], [tile[0], tile[1]+1]]        
-    
-if __name__ == '__main__':
-    file = "sokoban01.txt"
-    ai =AI((sokoban_game.Sokoban.readFile(file)))
-    locations =  ai.generateAllMoves(ai.startPosition, ai.board)
-    print(f"start location:{ai.startPosition}")
-    output = ai.board.tolist()
-    for loc in locations:
-        print(loc)
-        output[loc[0]][loc[1]] = '.'
-    output[ai.startPosition[0]][ai.startPosition[1]] = 'P'
+        ret= []
+        ret.append([tile[0] - 1, tile[1]] if tile[0] - 1 < self.board.shape[0] else None)
+        ret.append([tile[0] + 1, tile[1]] if tile[0] + 1 >= 0 else None)
+        ret.append([tile[0], tile[1] - 1] if tile[1] - 1 >= 0 else None)
+        ret.append([tile[0], tile[1] + 1] if tile[1] + 1 < self.board.shape[1] else None)
+        return ret
+
+    '''
+    given some board state with boxes and the player's position,
+    return a unique bitVector representation that will represent the state
+    of the game
+    currently size 6 -> 64 for array
+    '''
+    def getState(self, board, position):
+        bvBox = BitVector(size = 0)
+        POSITION_SIZE = 6
+        for index, x in np.ndenumerate(board):
+            if( x == g.BOXES):
+                bvBox = bvBox + BitVector(intVal=1)
+            else:
+                bvBox = bvBox + BitVector(intVal=0)
+        
+        bvPos = BitVector(intVal = position[0], size = POSITION_SIZE) + BitVector(intVal = position[1], size = POSITION_SIZE)
+        ret = bvBox + bvPos
+        ret.pad_from_right(8 -ret.length()%8)
+        hexString = ret.get_bitvector_in_ascii()
+        return hexString
+
+def printBoard(board, potentialMoves, playerLocation):
+    output = board.tolist()
+    if potentialMoves:
+        for loc in potentialMoves:
+            output[loc[0]][loc[1]] = '.'
+    output[playerLocation[0]][playerLocation[1]] = 'P'
     for i in output:
         for j in i:
             if j != 0:
@@ -190,6 +220,14 @@ if __name__ == '__main__':
             else:
                 print(" ",end="")
         print()
+
+if __name__ == '__main__':
+    file = "sokoban01.txt"
+    ai =AI((sokoban_game.Sokoban.readFile(file)))
+    locations, position =  ai.generateAllMoves(ai.startPosition, ai.board)
+    print(f"start location:{ai.startPosition}")
+    print("Starting board")
+    printBoard(ai.board, None ,ai.startPosition)
     print(ai.dfsSolver())
 
 
