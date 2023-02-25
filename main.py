5bd7666ff17c62522bcaaa2182a99b4b17890395
Arthur D Siu <51488740+arthurdsiu@users.noreply.github.com>
2023-02-24 17:49:04 -0800

    DFS with state space representation and count deadlock detection (#5)
    * updates so you can load a move history
    
    * finding current move space
    
    * workish, but need state space representation
    
    * debugging in process checkpoint
    
    * fixed the bug
    
    * debugging deadlock feature
    
    * debugging deadlock. single deadlock working
    
    * fixed bug
    
    * all tables put together, seems to be a bug with marking good cells and dead.
    
    * bug fixed for good cells marked as bad
    
    * working dfs with box count, but some bug allows boxes to move to dead zones
    
    * bug with counters
    
    * count deadlock fixed
    
    * optimzations for sokoban_game
    
    * update gitignore
    
    * driver changes
    
    * gitignore updates
    
    * off by one error
    
    * hopefully fixed the bug
    
    * working on debugg compressed path into moves
    
    * bug fix for compressed path to moves. Also put back in box detection for BFS since w/o it, it phases through boxes.

--- main.py
+++ main.py
@@ -6,22 +6,37 @@ from sokoban_game import Sokoban
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
-        str  = input("Please enter file location: ")
+        str  = input("No file location provided in cmdline\nPlease enter file location: ")
         print(str)
-        result, board = read_sokoban(str)
+        game = Sokoban(str)
     else:
-        result, board = read_sokoban(sys.argv[1])
-    #print(result)
-    #print(board)
-    #n_boxes = result["n_boxes"]
-    #print(f"The number of boxes in this sokoban is {n_boxes}")
-    game = Sokoban(board)
+        game = Sokoban(sys.argv[1])
+    
     cont = True
     while(cont):
         if game.completed:
-            print("you won!")
+            print("you won! Here's your move History")
+            print(game.moveHistory)
+            outputLocation = "move.txt"
+            print(f"writing move history to {outputLocation}")
+            try:
+                with open(outputLocation, "w") as f:
+                    f.write(game.moveHistory)
+            except FileNotFoundError:
+                print("Error Writing to File")
             exit()
         char = getch.getch()
+        print("l to load a move file to auto play:\
+\nq to go back\
+\nwasd to navigate the maze")
+        if(char == 'l'):
+            input_location = input("Enter move file: ")
+            try:
+                with open(input_location, "r") as f:
+                    input_lines = [i.strip() for i in f.readlines()]
+            except FileNotFoundError:
+                 print("Error Accessing File")
+            game.autoMove(input_lines[0])
         if(char == 'q'):
             cont = False
         if(char == 'w'):
